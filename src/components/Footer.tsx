8455720b67af170d1b45e65cdb232f668f21f298
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2026-01-08 21:53:53 +0100

    Footer v0

---
+++ src/components/Footer.tsx
@@ -0,0 +1,37 @@
+import '../styles/Footer.css'
+import { FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+
+function Footer() {
+  return (
+    <footer className='Footer'>
+        <div className='Footer_Top'>
+        <a href="/contact" className="Footer_Contact">
+            CONTACT US
+        </a>
+        </div>
+        <div className='Footer_Middle'>
+        <a
+            href="https://www.instagram.com/thekahicompany/"
+            target='_blank'
+            rel="noopener noreferrer"
+            aria-label='Instagram'
+          >
+            <FaInstagram/>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/the-kahi-company-687303112/"
+            target='_blank'
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+          >
+            <FaLinkedinIn/>
+          </a>
+        </div>
+        <div className='Footer_Bottom'>
+            <p>© 2019-2026 The Kahi Company TM LLC, All rights reserved</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
