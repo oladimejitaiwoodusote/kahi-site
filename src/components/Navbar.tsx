1b77dbdb5dcbdb99f8606f5d4ea4106f2e548142
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2026-01-19 18:12:46 +0100

    Responsiveness navbar v0

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,52 +1,48 @@
 import '../styles/Navbar.css';
 import logo from '../assets/Logo.jpg';
-import { FaInstagram, FaLinkedinIn, FaFacebook } from 'react-icons/fa';
+import { FaInstagram, FaLinkedinIn, FaFacebook, FaBars, FaTimes } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className='Navbar'>
-        <div className='Navbar_logo'>
-          <Link to="/">         
-            <img src={logo} alt="The Kahi Company Logo"/>
-          </Link>
-        </div>
+      <div className='Navbar_logo'>
+        <Link to="/">
+          <img src={logo} alt="The Kahi Company Logo" />
+        </Link>
+      </div>
 
-        <nav className='Navbar_middle'>
-          <a href="/">HOME</a>
-          <a href="/about">ABOUT US</a>
-          <a href="/services">SERVICES</a>
-          <a href="/contact">CONTACT US</a>
-        </nav>
+      {/* Desktop nav */}
+      <nav className={`Navbar_middle ${menuOpen ? 'open' : ''}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>HOME</Link>
+        <Link to="/about" onClick={() => setMenuOpen(false)}>ABOUT US</Link>
+        <Link to="/services" onClick={() => setMenuOpen(false)}>SERVICES</Link>
+        <Link to="/contact" onClick={() => setMenuOpen(false)}>CONTACT US</Link>
 
-        <div className='Navbar_right'>
-          <a
-            href="https://www.instagram.com/thekahicompany/"
-            target='_blank'
-            rel="noopener noreferrer"
-            aria-label='Instagram'
-          >
-            <FaInstagram/>
-          </a>
-          <a
-            href="https://www.facebook.com/Thekahicompany/"
-            target='_blank'
-            rel="noopener noreferrer"
-            aria-label='Facebook'
-          >
-            <FaFacebook/>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/the-kahi-company-687303112/"
-            target='_blank'
-            rel="noopener noreferrer"
-            aria-label="LinkedIn"
-          >
-            <FaLinkedinIn/>
-          </a>
+        {/* Socials inside mobile menu */}
+        <div className="Navbar_mobile_socials">
+          <a href="https://www.instagram.com/thekahicompany/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+          <a href="https://www.facebook.com/Thekahicompany/" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+          <a href="https://www.linkedin.com/in/the-kahi-company-687303112/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></a>
         </div>
+      </nav>
+
+      {/* Desktop socials */}
+      <div className='Navbar_right'>
+        <a href="https://www.instagram.com/thekahicompany/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+        <a href="https://www.facebook.com/Thekahicompany/" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+        <a href="https://www.linkedin.com/in/the-kahi-company-687303112/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></a>
+      </div>
+
+      {/* Hamburger */}
+      <button className="Navbar_toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </button>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
